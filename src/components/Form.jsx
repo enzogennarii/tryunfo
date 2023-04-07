6df1a8646de7d1c4035f0da68674608f3e0883bd
Enzo Gennari <enzo.gennari02@gmail.com>
2023-04-06 23:54:51 -0300

    Conclui requisito 1

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,61 @@ import React, { Component } from 'react';
 class Form extends Component {
   render() {
     return (
-      <div>Form</div>
+      <>
+        <h2>Adicione nova carta</h2>
+        <form>
+          <label htmlFor="name-input">
+            Nome
+            <input type="text" id="name-input" data-testid="name-input" />
+          </label>
+
+          <label htmlFor="description-input">
+            Descrição
+            <textarea
+              id="description-input"
+              cols="30"
+              rows="10"
+              data-testid="description-input"
+            />
+          </label>
+
+          <label htmlFor="attr1-input">
+            Attr01
+            <input type="number" id="attr1-input" data-testid="attr1-input" />
+          </label>
+
+          <label htmlFor="attr2-input">
+            Attr02
+            <input type="number" id="attr2-input" data-testid="attr2-input" />
+          </label>
+
+          <label htmlFor="attr3-input">
+            Attr03
+            <input type="number" id="attr3-input" data-testid="attr3-input" />
+          </label>
+
+          <label htmlFor="image-input">
+            Imagem
+            <input type="text" id="image-input" data-testid="image-input" />
+          </label>
+
+          <label htmlFor="rare-input">
+            Raridade
+            <select id="rare-input" data-testid="rare-input">
+              <option value="normal">Normal</option>
+              <option value="raro">Raro</option>
+              <option value="muito raro">Muito raro</option>
+            </select>
+          </label>
+
+          <label htmlFor="trunfo-input">
+            Imagem
+            <input type="checkbox" id="trunfo-input" data-testid="trunfo-input" />
+          </label>
+
+          <button id="btn-submit" data-testid="save-button">Salvar</button>
+        </form>
+      </>
     );
   }
 }
